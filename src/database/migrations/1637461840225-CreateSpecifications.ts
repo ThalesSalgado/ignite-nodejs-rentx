481989d33882fa27773163b72fbda07676ee9e3b
ThalesSalgado-PS <tqi_tdutra@uolinc.com>
2021-11-20 23:38:24 -0300

    NodeJS - Ch3 - Migration para Specifications; Refatorando repository de specification

---
+++ src/database/migrations/1637461840225-CreateSpecifications.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateSpecifications1637461840225 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(
+      new Table({
+        name: "specifications",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "id",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
