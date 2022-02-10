c0e92f7d13e32afd9fa7daf2dcb257a52333156f
ThalesSalgado-PS <tqi_tdutra@uolinc.com>
2022-02-09 22:51:22 -0300

    Ch3; Pt 5; Upload Avatar de Usuario

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,27 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
 import { ListUsersController } from "../modules/accounts/useCases/listUsers/ListUsersController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 const createUserController = new CreateUserController();
 const listUsersController = new ListUsersController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+);
 
 usersRoutes.get("/", listUsersController.handle);
 
