aeebbfb266940a12f64c7aaae18995bc964716d5
ThalesSalgado-PS <tqi_tdutra@uolinc.com>
2022-03-11 10:55:22 -0300

    Ch 4; Pt 3; Finalizando caso de uso de criacao de especificacao de carro - CreateCarSpecificationUseCase

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -1,16 +1,20 @@
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { SpecificationsRepositoryInMemory } from "@modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory";
 import { AppError } from "@shared/errors/AppError";
 
 import { CreateCarSpecificationUseCase } from "./CreateCarSpecificationUseCase";
 
 let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
 let carsRepositoryInMemory: CarsRepositoryInMemory;
+let specificationsRepositoryInMemory: SpecificationsRepositoryInMemory;
 
 describe("Create Car Specification", () => {
   beforeEach(() => {
     carsRepositoryInMemory = new CarsRepositoryInMemory();
+    specificationsRepositoryInMemory = new SpecificationsRepositoryInMemory();
     createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
-      carsRepositoryInMemory
+      carsRepositoryInMemory,
+      specificationsRepositoryInMemory
     );
   });
 
@@ -36,11 +40,20 @@ describe("Create Car Specification", () => {
       brand: "Brand",
       category_id: "category",
     });
-    const specifications_id = ["54321"];
 
-    await createCarSpecificationUseCase.execute({
+    const specification = await specificationsRepositoryInMemory.create({
+      description: "test",
+      name: "test",
+    });
+
+    const specifications_id = [specification.id];
+
+    const specificationsCars = await createCarSpecificationUseCase.execute({
       car_id: car.id,
       specifications_id,
     });
+
+    expect(specificationsCars).toHaveProperty("specifications");
+    expect(specificationsCars.specifications.length).toBe(1);
   });
 });
