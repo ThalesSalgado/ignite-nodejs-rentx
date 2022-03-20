1377f97e66925cd6a698421b555065574ed1991e
ThalesSalgado-PS <tqi_tdutra@uolinc.com>
2022-03-19 23:03:53 -0300

    Ch 4; Pt 4; Controller aluguel de carros

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,5 +1,13 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+@Entity("rentals")
 class Rental {
   constructor() {
     if (!this.id) {
@@ -7,22 +15,31 @@ class Rental {
     }
   }
 
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   car_id: string;
 
+  @Column()
   user_id: string;
 
+  @Column()
   start_date: Date;
 
+  @Column()
   end_date: Date;
 
+  @Column()
   expected_return_date: Date;
 
+  @Column()
   total: number;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
